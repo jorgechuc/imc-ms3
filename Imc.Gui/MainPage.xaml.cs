43999f90dce876c7e0dbf3359e6ab4dcb5399986
Jorge Luis Chuc Lopez <jorge.cl@campeche.tecnm.mx>
2025-10-08 11:48:52 -0600

    Agrega comportamiento a la interfaz de usuario

--- Imc.Gui/MainPage.xaml.cs
+++ Imc.Gui/MainPage.xaml.cs
@@ -5,6 +5,55 @@
 		public MainPage()
 		{
 			InitializeComponent();
+
+			PesoEntry.Text = string.Empty;
+			EstaturaEntry.Text = string.Empty;
+			ImcLabel.Text = string.Empty;
+			SituacionNutricionalLabel.Text = string.Empty;
 		}
+
+		private void OnCalcularButtonClicked(object sender, EventArgs e)
+		{
+			decimal peso = Convert.ToDecimal(PesoEntry.Text);
+			decimal estatura = Convert.ToDecimal(EstaturaEntry.Text);
+			decimal imc = IndiceDeMasaCorporal(peso, estatura);
+			ImcLabel.Text = imc.ToString("G6");
+			SituacionNutricionalLabel.Text = DeterminaEstadoNutricional(imc);
+		}
+
+		private void OnLimpiarButtonClicked(object sender, EventArgs e)
+		{
+			PesoEntry.Text = string.Empty;
+			EstaturaEntry.Text = string.Empty;
+			ImcLabel.Text = string.Empty;
+			SituacionNutricionalLabel.Text = string.Empty;
+		}
+
+		private decimal IndiceDeMasaCorporal(decimal peso,  decimal estatura)
+		{
+			return peso / (estatura * estatura);
+		}
+
+		private string DeterminaEstadoNutricional(decimal imc)
+		{
+			if (imc < 18.5m)
+			{
+				return "Peso bajo";
+			}
+			if (imc < 25.00m) 
+			{
+				return "Peso normal";
+			}
+			if (imc < 30.0m)
+			{
+				return "Sobrepeso";
+			}
+			if (imc < 40.0m)
+			{
+				return "Obesidad";
+			}
+			return "Obesidad extrema";
+		}
+
 	}
 }
